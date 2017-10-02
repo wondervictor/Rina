42c4a4e4775a9366aaf3249696df2cbeb5e90785
wondervictor <victorchanchina@gmail.com>
2017-10-02 23:52:57 +0800

    iniy

---
+++ socket/Socket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 程天恒 on 2017/10/2.
+//
+
+#include "Socket.h"
---
+++ socket/Socket.h
@@ -0,0 +1,20 @@
+//
+// Created by 程天恒 on 2017/10/2.
+//
+
+#ifndef RINA_SOCKET_H
+#define RINA_SOCKET_H
+
+namespace Rina {
+
+    class Socket {
+
+        
+
+
+    };
+
+}
+
+
+#endif //RINA_SOCKET_H
