00466c3caf97d3e7acc56f128a3682ff3da3e817
geniusrabbit <U201613566@hust.edu.cn>
2017-10-24 23:08:09 +0800

    liu

--- socket/Socket.cpp
+++ socket/Socket.cpp
@@ -36,14 +36,14 @@ int ServerSocket::init(int port) {
   serverSockAddr.sin_addr.s_addr = INADDR_ANY;
   serverSockAddr.sin_port=htons(port);
   serverSockAddr.sin_family = AF_INET;
-  bzero((serverSockAddr.sin_zero), 8);
-  this->port = port;
+//  bzero((serverSockAddr.sin_zero), 8);
+  this->port = htons(port);
   this->sockfd = sockfd;
   this->serverAddr = serverSockAddr;
-  int flag = bind(sockfd, (sockaddr* )&serverSockAddr, sizeof(sockaddr));
+  int flag = bind(sockfd, (sockaddr* )&serverSockAddr, sizeof(serverSockAddr));
   CHECK(flag, SOCKET_ERROR, LOG_ERROR("Server Socket Bind Failed"))
   LOG_INFO("Server Socket Init Success")
-  return 1;
+  return sockfd;
 }
 
 // start server
@@ -51,6 +51,11 @@ int ServerSocket::startServer() {
   LOG_INFO("Stating The Server...")
   // Max Connecting
   int flag = listen(this->sockfd, this->maxConn);
+
+  printf("flag:%d\n",flag);
+
+  //printf("this->sockfd=%d--this->maxConn=%d\n",this->sockfd, this->maxConn);
+
   CHECK(flag, SOCKET_ERROR, LOG_ERROR("Server Socket Listen Failed"));
   return 1;
 }
@@ -63,14 +68,17 @@ int ServerSocket::sendMessage(int sockfd, void *buf, size_t size) {
 }
 
 long ServerSocket::recvMessage(int sockfd, void *buf, size_t size) {
+  //
   ssize_t recvSize = recv(sockfd, buf, size, 0);
   CHECK(recvSize, SOCKET_ERROR, LOG_ERROR("Server Socket Recv Error"))
   return recvSize;
 }
 
 int ServerSocket::acceptConn(sockaddr_in *clientAddr) {
-  int sinsize;
+  int sinsize = sizeof(struct sockaddr_in);
   int clientfd = accept(this->sockfd, (sockaddr* )&clientAddr, (socklen_t* )&sinsize);
+  //
+  printf("clientfd:%d\n",clientfd);
   CHECK(clientfd, SOCKET_ERROR, LOG_ERROR("Server Accept Error"))
   this->clients.push_back(clientfd);
   LOG_INFO("Server Add Client: %d", clientfd)
@@ -102,6 +110,7 @@ std::vector<int> ServerSocket::getClients() {
 
 
 int ClientSocket::init(int port) {
+
   in_addr_t clientIP = inet_addr("127.0.0.1");
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   CHECK(sockfd, -1, LOG_ERROR("Client Socket Init Failed"))
@@ -110,11 +119,14 @@ int ClientSocket::init(int port) {
   clientSockAddr.sin_port = htons(port);
   clientSockAddr.sin_family = AF_INET;
   clientSockAddr.sin_addr.s_addr = clientIP;
+
   int flag = bind(sockfd, (sockaddr* )&clientSockAddr, sizeof(sockaddr));
   CHECK(flag, SOCKET_ERROR, LOG_ERROR("Client Socket Bind Error"))
+
   this->sockfd = sockfd;
   this->port = port;
   this->clientAddr = clientSockAddr;
+
   LOG_INFO("Client Socket Init Success")
   return 1;
 }
@@ -139,13 +151,15 @@ int ClientSocket::stop() {
 }
 
 int ClientSocket::conn(const std::string& serverIPStr, int port) {
+  //in_addr_t serverIP = inet_addr(serverIPStr.c_str());
   in_addr_t serverIP = inet_addr(serverIPStr.c_str());
   sockaddr_in server;
   server.sin_addr.s_addr = serverIP;
   server.sin_family = AF_INET;
   server.sin_port = htons(port);
 
-  int flag = connect(this->sockfd, (sockaddr* )&server, sizeof(sockaddr));
+  int flag = connect(this->sockfd, (sockaddr*)&server, sizeof(sockaddr));
+  printf("flag:%d\n",flag);
   CHECK(flag, SOCKET_ERROR, LOG_ERROR("Client Connect %s Failed", serverIPStr.c_str()))
   LOG_INFO("Client Connect %s Successed", serverIPStr.c_str())
   this->connectAddr = server;
